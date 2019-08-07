b968079e33e6a47e8f6cab655c70ecfe8be8fb71
Nani <Nani@Nani-PC>
2019-08-08 00:02:50 +0530

    First commit java code

---
+++ Programs/src/com/java/datastructurs/Multiply.java
@@ -0,0 +1,15 @@
+package com.java.datastructurs;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int x =2;
+		int y = 3;
+		int z;
+		
+		z = x*y;
+		
+		System.out.println(z);
+	}
+
+}
